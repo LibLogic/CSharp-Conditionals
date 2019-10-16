d6c25ec8624b047a0539fdfb1dcbe11058acf124
Tom Hodgkinson <hdgknsn@gmail.com>
2019-10-16 11:28:52 -0400

    added a challenge

---
+++ Loops.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+    public class Loops
+    {
+        public static int DivByThree()
+        {
+            int count = 0;
+            for(int i = 1; i <= 100; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static int SumInputs()
+        {
+            int sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Please enter some numbers, preess \"ok\" when done.");
+                string input = Console.ReadLine();
+                if (input == "ok")
+                {
+                    return sum;
+                }
+                sum += int.Parse(input);
+            }
+        }
+
+        public static int CalculateFactorial(int num)
+        {
+            int factorial = 1;
+            for (int i = 1; i < num; i++)
+            {
+                factorial = factorial * (i + 1);
+            }
+           return factorial;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 
 namespace CSharpFundamentals
-{ 
+{
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Conditionals.IsValid());
-            Console.WriteLine(Conditionals.DisplayMax());
-            Console.WriteLine(Conditionals.Orientation());
-            Console.WriteLine(Conditionals.SpeedTrap());
+//            Console.WriteLine(Conditionals.IsValid());
+//            Console.WriteLine(Conditionals.DisplayMax());
+//            Console.WriteLine(Conditionals.Orientation());
+//            Console.WriteLine(Conditionals.SpeedTrap());
+
+            Console.WriteLine(Loops.DivByThree());
+            Console.WriteLine(Loops.SumInputs());
+            Console.WriteLine(Loops.CalculateFactorial(5));
         }
     }
 }
